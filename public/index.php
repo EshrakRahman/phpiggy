3b1df172c0eabe586a2b087b5fa096e66070f6cb
Eshrak Rahman <eshrakurrahman@gmail.com>
2025-08-31 00:33:12 +0600

    router class finished

--- public/index.php
+++ public/index.php
@@ -1,10 +1,8 @@
 <?php
 
-include_once __DIR__."/../src/App/functions.php";
+include_once __DIR__ . "/../src/App/functions.php";
 include __DIR__ . "/../src/App/bootstrap.php";
-echo __DIR__ . "/../src/App/bootstrap.php";
 
 
-dd($app);
 
 $app->run();
