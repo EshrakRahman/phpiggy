550a1d9102399b7e93cdaaab1e08fb851b9f4597
Eshrak Rahman <eshrakurrahman@gmail.com>
2025-09-02 23:04:16 +0600

    control

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -3,11 +3,23 @@
 declare(strict_types=1);
 
 namespace App\Controllers;
+use Framework\TemplateEngine;
+use App\Config\Path;
+
 
 class HomeController
 {
+    private TemplateEngine $view;
+
+    public function __construct() 
+    {
+        $this->view = new TemplateEngine(Path::VIEW);
+    }
     public function home()
     {
-        echo "home page";
+        echo $this->view->render("/index.php", [
+            "title" => "Home"
+        ]);
+        
     }
 }
--- src/Framework/App.php
+++ src/Framework/App.php
@@ -8,17 +8,17 @@ class App
 {
 
     private Router $router;
+    private Container $container;
 
     public function __construct()
     {
         $this->router = new Router();
+        $this->container = new Container();
     }
     public function run()
     {
         $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
         $method = $_SERVER["REQUEST_METHOD"];
-        echo $path;
-
         $this->router->dispatch($path, $method);
     }
 
