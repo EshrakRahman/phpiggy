e8d868732673ca5c641681c6010608cb772d04a7
Eshrak Rahman <eshrakurrahman@gmail.com>
2025-09-19 21:33:14 +0600

    transaction completed

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -3,21 +3,23 @@
 declare(strict_types=1);
 
 namespace App\Controllers;
-use Framework\TemplateEngine;
-use App\Config\Path;
 
+use Framework\TemplateEngine;
+use App\Services\TransactionService;
 
 class HomeController
 {
-    
 
-    public function __construct(private TemplateEngine $view) 
+
+    public function __construct(private TemplateEngine $view, private TransactionService $transactionService)
     {
-
     }
     public function home()
     {
-        echo $this->view->render("/index.php");
-        
+        $transaction = $this->transactionService->getUserTransactions();
+        echo $this->view->render(
+            "/index.php",
+            ["transactions" => $transaction]
+        );
     }
 }
